c11fe31a48db94bc2e28d5f661fb59e950abaf99
GIANCARLO JOEL ALVAREZ GALLARDO <201920420@urp.edu.pe>
2025-02-22 11:14:17 -0500

    Cambio de tiutlo pagina principal dashboard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div>
       <h1>Inicio</h1>
-      <p>Bienvenido a la página de inicio.</p>
+      <p>Bienvenido a la página de inicio del dashboard</p>
     </div>
   );
 };
