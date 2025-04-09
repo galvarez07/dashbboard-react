58c39b686ecfae27f5e03460c6d1727193c85b73
GIANCARLO JOEL ALVAREZ GALLARDO <201920420@urp.edu.pe>
2025-04-09 16:33:30 -0500

    Cambio de Titulo Ejemplo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+
 
 const Home = () => {
   return (
     <div>
       <h1>Inicio</h1>
-      <p>Bienvenido a la página de HOME del dashboard en REACT JS</p>
+      <p>Bienvenido a la página de HOME del dashboard en REACT JS con Node JS</p>
     </div>
   );
 };
