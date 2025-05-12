5d47b063154132ebc9721cd8a329918383bed218
Giancarlo Joel Alvarez Gallardo <galvarezga09@outlook.com>
2025-05-11 23:30:05 -0500

    Cambio de Titutlo React Title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div>
       <h1>Inicio</h1>
-      <p>Bienvenido a la página de HOME del dashboard en REACT JS  con Javascript</p>
+      <p>Bienvenido a la página de HOME del dashboard en REACT JS  con JS</p>
     </div>
   );
 };
